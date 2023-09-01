c77cfa1fca89c5425a0f8ac0c88df30887fdfb0d
Sol Kim <solkim010101@gmail.com>
2023-09-01 15:20:38 +0900

    MemberService, MemberController_signIn, deleteMember

--- src/main/java/com/team7/be/domain/service/MemberService.java
+++ src/main/java/com/team7/be/domain/service/MemberService.java
@@ -1,18 +1,23 @@
 package com.team7.be.domain.service;
 
 
+import com.team7.be.domain.controller.request.member.SignInRequest;
 import com.team7.be.domain.controller.request.member.SignUpRequest;
 import com.team7.be.domain.entity.Member;
 import com.team7.be.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = false)
 public class MemberService {
     private final MemberRepository memberRepository;
+    private BCryptPasswordEncoder passwordEncoder;
 
 
     @Transactional
@@ -32,5 +37,30 @@ public class MemberService {
         }
         //member에 대한 정보를 저장하고있는 repo에서 입력받은 userid가 존재하는 지에 대한 여부를 판단.
         // 이미 member 정보가 존재할경우 이를 알림.
+    }
+
+
+    public Optional<Member> signIn(SignInRequest signInRequest) {
+        Optional<Member> findMember = memberRepository.findByUserId(signInRequest.getUserId());
+        if(findMember.isPresent()) {
+            Member member = findMember.get();
+            if (isPasswordValid(signInRequest.getPassword(), member.getUserPw())) {
+                return findMember;
+            }
+        }
+        return Optional.empty();
+    }
+
+    private boolean isPasswordValid(String inputPassword, String userPassword) {
+        return passwordEncoder.matches(inputPassword, userPassword);
+    }
+
+    public void deleteMember(Long id) {
+        Optional<Member> findMember = memberRepository.findById(id);
+        if (findMember.isPresent()) {
+            Member member = findMember.get();
+            memberRepository.delete(member);
+
+        } else throw new IllegalArgumentException("회원을 찾을 수 없습니다.");
     }
 }
