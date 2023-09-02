33c715b5d323acde63a83ce106771dacbf85c4b9
Kimdaemin <56399681+daemin-kim@users.noreply.github.com>
2023-09-02 12:34:30 +0900

    [feat] Implement ScheduleService for CRUD and filtering

--- src/main/java/com/team7/be/domain/repository/ScheduleRepository.java
+++ src/main/java/com/team7/be/domain/repository/ScheduleRepository.java
@@ -2,7 +2,6 @@ package com.team7.be.domain.repository;
 
 import com.team7.be.domain.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
@@ -14,6 +13,13 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     Optional<Schedule> findByGroupIdAndMemberId(@Param("groupId") Long groupId, @Param("MemberId") Long memberId);
 
     List<Schedule> findByGroupIdAndSelectedStartDateIsAfterAndSelectedEndDateIsBefore(
+            @Param("groupId") Long groupId,
+            @Param("selectedStartDate") LocalDateTime selectedStartDate,
+            @Param("selectedEndDate") LocalDateTime selectedEndDate
+    );
+
+    // 받은 날짜로부터 7일 후까지의 스케줄을 받아오는 메소드, selectedEndDate를 현재 날짜로부터 7일 후로 설정해야함.
+    List<Schedule> findByGroupIdAndSelectedStartDateIsAfterAndSelectedEndDateIsBeforeOrSelectedEndDateIsNull(
             @Param("groupId") Long groupId,
             @Param("selectedStartDate") LocalDateTime selectedStartDate,
             @Param("selectedEndDate") LocalDateTime selectedEndDate
