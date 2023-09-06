28bed75ddf02d63e5640c60be33ced60c32ff832
zaiha <45660311+215woghks@users.noreply.github.com>
2023-09-06 15:38:46 +0900

    controller 오류 수정

--- src/main/java/com/team7/be/domain/controller/GroupController.java
+++ src/main/java/com/team7/be/domain/controller/GroupController.java
@@ -1,6 +1,5 @@
 package com.team7.be.domain.controller;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,25 +7,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+
 import static org.springframework.web.util.UriComponentsBuilder.fromPath;
-import com.team7.be.domain.controller.request.member.SignUpRequest;
-import com.team7.be.domain.service.MemberService;
 
 
 @RequiredArgsConstructor
 @RestController
-public class MemberController {
+public class GroupController {
     private final MemberService memberService;
 
     @PostMapping("/signUp")
     public ResponseEntity<Void> signUp(@RequestBody SignUpRequest signUpRequest){
         String  userId = memberService.signUp(signUpRequest);
-        //service의 signUp 메소드를 사용하여 저장된 의 고유 id를 저장
         URI uri = fromPath("/member/{userId}")
                 .buildAndExpand(userId)
                 .toUri();
-        //회원가입 진행 시 사용자별로 식별할 수 있는 그들읠 userId를 uri에 지정하여 개별적인 endpoint로 이동할 수 있도록 한다.
-        return ResponseEntity.created(uri).build();
 
+        return ResponseEntity.created(uri).build();
+        }
     }
-}
+
